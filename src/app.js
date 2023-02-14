8aebcbcf2b679e07b1aac510a16d071f1213a9f7
Sara Enríquez <sara_enriquezdesalamanca@hotmail.com>
2023-02-14 21:13:20 +0000

    creado domain generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let domain = ["com", "net", "es"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+pronoun.forEach(element => {
+  adj.forEach(adjetive => {
+    noun.forEach(item => {
+      domain.forEach(dom => {
+        console.log(`${element}${adjetive}${item}.${dom}`);
+      });
+    });
+  });
+});
